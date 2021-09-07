67575de982afedc86abf8ac466053a3657f40ef0
Jake Levi <39222075+towrofterra@users.noreply.github.com>
2021-09-06 21:37:30 -0400

    Added GUI skeleton

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,29 @@
+import javax.swing.*;
+
 public class Game {
     int tick = 0;
+    private JPanel rootPanel;
+    private JLabel name;
+    private JButton button1;
+    private JProgressBar progressBar1;
+    private JProgressBar progressBar2;
+    private JProgressBar progressBar3;
+    private JButton button2;
+    private JButton button3;
+    private JLabel Generators;
+    private JLabel Resources;
 //    Player p;
 
     public static void main(String[] args){
+        JFrame frame = new JFrame("Idle Thief");
+        frame.setContentPane(new Game().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+
+
+
         Player p1 = new Player("Jake");
         System.out.println(p1.getStatus());
 
